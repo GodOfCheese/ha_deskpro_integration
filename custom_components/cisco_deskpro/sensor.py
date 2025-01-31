4e67e750f6eb1818ff7d535e2512dfb3cc13489a
God of Cheese <cheddardeity@hotmail.com>
2025-01-31 15:56:07 -0800

    HACS compat bugs

--- custom_components/cisco_deskpro/sensor.py
+++ custom_components/cisco_deskpro/sensor.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 from datetime import timedelta
 
-from deskpro import Deskpro 
+from .deskpro import Deskpro 
 
 from homeassistant.components.sensor import (
     SensorDeviceClass,
