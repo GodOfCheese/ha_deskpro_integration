b60d7aaa6a44cc4f1fa6b9e96f9f0878006187bd
GodOfCheese <cheddardeity@hotmail.com>
2025-02-21 18:12:40 -0800

    Expose Alarm and RoomInUse

--- custom_components/cisco_deskpro/sensor.py
+++ custom_components/cisco_deskpro/sensor.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 from datetime import timedelta
-
+import logging
 from .deskpro import Deskpro 
 
 from homeassistant.components.sensor import (
@@ -11,14 +11,14 @@ from homeassistant.components.sensor import (
     SensorStateClass,
 )
 
-from homeassistant.components.binary_sensor import BinarySensorEntity
-from homeassistant.components.binary_sensor import DEVICE_CLASS_OCCUPANCY
+from homeassistant.components.binary_sensor import BinarySensorEntity, BinarySensorDeviceClass
 from homeassistant.const import UnitOfTemperature, UnitOfSoundPressure
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_VERIFY_SSL, CONF_PASSWORD
 
+logger = logging.getLogger(__name__)
 
 def setup_platform(
     hass: HomeAssistant,
@@ -42,7 +42,7 @@ def setup_platform(
             AmbientNoiseLevel(dpx),
             CurrentSoundLevel(dpx),
             RoomInUse(dpx),
-            # AlarmDetected(dpx),
+            AlarmDetected(dpx),
             PeopleCount(dpx),
             Humidity(dpx),
         ]
@@ -141,18 +141,15 @@ class PeopleCount(DeskproSensor):
 
     pass
 
+class DeskproBinarySensor(BinarySensorEntity):
 
-class RoomInUse(BinarySensorEntity):
-    _attr_name = "Deskpro in use"
-    _attr_unique_id = "dpinuse"
-    _attr_state_class = SensorStateClass.OCCUPANCY
-    _attr_device_class = DEVICE_CLASS_OCCUPANCY
-    _attr_should_poll = False
+    _attr_state_class = None
+    _attr_should_poll = True
+    # You must set name, uid, device class.
 
-    def __init__(self, dp: Deskpro):
-
+    def __init__(self, attr:str, dp:DeskproXMLSensor):
         self.dp = dp
-        self._name = "Deskpro in use"
+        self.attr = attr
         return
     
     @property
@@ -160,21 +157,46 @@ class RoomInUse(BinarySensorEntity):
         return self._name
     
     @property
-    def is_on(self):
-        return self.dp.status["RoomInUse"] == "true"
+    def is_on(self) -> bool:
+
+        logger.debug( f"Deskpro state: {self.dp.dp.status}")
+
+        current = self.dp.get(self.attr)
+
+        if current is None:
+            # BUGBUG: how do I say "unavailable?"
+            return False
+
+        if current.lower() == "true":
+            return True
+        elif current.lower() == "false":
+            return False
+        else:
+            raise Exception(f"Unexpected value for {attr}: " + current)
+        return
+    pass # class
     
-    def update(self):
-        # do nothing.
+    
+class RoomInUse(DeskproBinarySensor):
+    _attr_name = "Deskpro in use"
+    _attr_unique_id = "dpinuse"
+    _attr_device_class = BinarySensorDeviceClass.OCCUPANCY
+
+    def __init__(self, dp: DeskproXMLSensor):
+        super().__init__(attr="RoomInUse", dp=dp )
+        self._name = "Deskpro in use"
         return
     pass
 
 
-class AlarmDetected(DeskproSensor):
+class AlarmDetected(DeskproBinarySensor):
     _attr_name = "T3 Alarm Detected"
     _attr_unique_id = "dpt3detect"
+    _attr_device_class = BinarySensorDeviceClass.SAFETY
 
-    def __init__(self, dp: Deskpro):
-        super().__init__(dp, "T3AlarmDetected")
+    def __init__(self, dp: DeskproXMLSensor):
+        super().__init__(attr="T3AlarmDetected", dp=dp)
+        self._name="T3 Alarm Detected"
         return
 
     pass
